3ef1e052083a4b835c1c462ba9ec0c83b3152c39
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-02-07 21:50:15 +0100

    Connection BD

---
+++ WinGo/src/main/java/Services/PersonneCRUD.java
@@ -0,0 +1,69 @@
+package Services;
+import Entites.Personne;
+import Utils.MyBD;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonneCRUD implements IntrefaceCRUD<Personne>{
+
+    Connection conn;
+
+    public PersonneCRUD() {
+        conn= MyBD.getInstance().getConn();
+    }
+    @Override
+    public void ajouter(Personne personne) throws SQLException {
+        String req=
+                "insert into personne (nom, prenom,age) " +
+                        "values('" + personne.getNom() + "','"
+                        + personne.getPrenom() + "'"
+                        +  "," + personne.getAge() + ")";
+
+        Statement st=conn.createStatement();
+        st.executeUpdate(req);
+       System.out.println("Personne ajoutée !");
+    }
+
+    @Override
+    public void modifier(Personne personne) throws SQLException {
+        String req="update personne set nom=?,prenom=?,age=? where id=?";
+
+    PreparedStatement pst=conn.prepareStatement(req);
+    pst.setString(1, personne.getNom());
+    pst.setString(2, personne.getPrenom());
+    pst.setInt(3, personne.getAge());
+    pst.setInt(4, personne.getId());
+    pst.executeUpdate();
+        System.out.println("Personne modifiée");
+    }
+
+    @Override
+    public void supprimer(int id) throws SQLException {
+        String req="delete from personne where id=?";
+        PreparedStatement pst=conn.prepareStatement(req);
+        pst.setInt(1, id);
+        pst.executeUpdate();
+        System.out.println("Personne supprimer");
+    }
+
+    @Override
+    public List<Personne> afficher() throws SQLException {
+        String req ="select * from personne";
+        Statement st=conn.createStatement();
+        ResultSet rs=st.executeQuery(req);
+        List<Personne> listepersonnes=new ArrayList<Personne>();
+
+        while(rs.next()){
+            Personne p = new Personne();
+            p.setId(rs.getInt("id"));
+            p.setNom(rs.getString("nom"));
+            p.setPrenom(rs.getString("prenom"));
+            p.setAge(rs.getInt("age"));
+
+            listepersonnes.add(p);
+        }
+        return listepersonnes;
+    }
+}
---
+++ WinGo/src/main/java/Tests/MainConnection.java
@@ -0,0 +1,27 @@
+package Tests;
+
+import Entites.Blog;
+import Entites.Commentaire;
+import Utils.MyBD;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+public class MainConnection {
+
+    public static void main(String[] args) throws SQLException {
+
+        MyBD myBD = MyBD.getInstance();
+
+        // Création des objets Blog
+        Blog b1 = new Blog("Sahara Adventure", "Voyage inoubliable à Douz", "douz.jpg", "Admin", "Sud", "Voyage");
+        Blog b2 = new Blog("Carthage Story", "Découverte historique de Carthage", "carthage.jpg", "Admin", "Nord", "Histoire");
+
+        // Création des objets Commentaire
+        Commentaire c1 = new Commentaire("Super article !", new Date(), 5, "Alice");
+        Commentaire c2 = new Commentaire("Jadore cet endroit !", new Date(), 6, "Bob");
+
+
+
+    }
+}
