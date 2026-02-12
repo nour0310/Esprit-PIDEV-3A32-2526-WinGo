eebb592853aba32325c4c27bd3c48401055062a6
Yomna Helali <yomna.helali@esprit.tn>
2026-02-12 13:33:35 +0100

    reglage interface

--- src/main/java/Controlles/ListeProduitsController.java
+++ src/main/java/Controlles/ListeProduitsController.java
@@ -1,4 +1,9 @@
 package Controlles;
 
+import javafx.fxml.FXML;
+
 public class ListeProduitsController {
+    @FXML private void goAjouter(){ Navigator.goTo("AjouterProduit.fxml","Ajouter Produit"); }
+    @FXML private void goListe(){ Navigator.goTo("ListeProduits.fxml","Liste Produits"); }
+    @FXML private void goDashboard(){ Navigator.goTo("Dashboard.fxml","Dashboard"); }
 }
