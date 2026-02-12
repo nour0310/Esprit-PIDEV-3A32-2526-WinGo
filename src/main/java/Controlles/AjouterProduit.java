26941987d80736cb006588caa38bddc4f9b4235e
Yomna Helali <yomna.helali@esprit.tn>
2026-02-12 14:13:24 +0100

    reglage interface

--- src/main/java/Controlles/AjouterProduit.java
+++ src/main/java/Controlles/AjouterProduit.java
@@ -7,7 +7,8 @@ import Entites.Produit;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
 public class AjouterProduit {
 
     @FXML private ResourceBundle resources;
@@ -23,10 +24,25 @@ public class AjouterProduit {
         // TODO: produit.setPrix(Double.parseDouble(prixTextField.getText()));
     }
 
+    @FXML private TextField idCommercantField;
+    @FXML private TextArea descriptionArea;
+    @FXML private TextField stockField;
+    @FXML private TextField regionField;
+    @FXML private TextField categorieField;
+    @FXML private TextField imageField;
+    @FXML private javafx.scene.control.Label statusLabel;
+
     @FXML
     void clearForm(ActionEvent event) {
-        nomTextField.clear();
-        prixTextField.clear();
+        if (idCommercantField != null) idCommercantField.clear();
+        if (nomTextField != null) nomTextField.clear();
+        if (descriptionArea != null) descriptionArea.clear();
+        if (prixTextField != null) prixTextField.clear();
+        if (stockField != null) stockField.clear();
+        if (regionField != null) regionField.clear();
+        if (categorieField != null) categorieField.clear();
+        if (imageField != null) imageField.clear();
+        if (statusLabel != null) statusLabel.setText("");
     }
 
     @FXML
