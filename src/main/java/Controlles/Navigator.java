ccb57f75dc3653a901f3c885ed09123fc7abe7a9
Yomna Helali <yomna.helali@esprit.tn>
2026-02-12 13:27:25 +0100

    reglage interface

--- src/main/java/Controlles/Navigator.java
+++ src/main/java/Controlles/Navigator.java
@@ -1,4 +1,30 @@
 package Controlles;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Navigator {
+    private static Stage stage;
+
+    public static void setStage(Stage primaryStage) {
+        stage = primaryStage;
+    }
+
+    public static void goTo(String fxml, String title) {
+        try {
+            Parent root = FXMLLoader.load(Navigator.class.getResource("/" + fxml));
+            if (stage.getScene() == null) {
+                stage.setScene(new Scene(root));
+            } else {
+                stage.getScene().setRoot(root); // switch page without creating new stage
+            }
+            stage.setTitle(title);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Cannot load: " + fxml + " => " + e.getMessage());
+        }
+    }
 }
