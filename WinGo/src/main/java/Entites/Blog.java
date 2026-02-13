07a1a02da345e7793f1cc931d9d08970d568cd7d
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-02-13 14:32:09 +0100

    Controllers

--- WinGo/src/main/java/Entites/Blog.java
+++ WinGo/src/main/java/Entites/Blog.java
@@ -1,18 +1,19 @@
 package Entites;
 
 public class Blog {
-    private int id_article;        // correspond à la colonne dans la base
+
+    private int id;          // correspond à article.id
     private String titre;
     private String contenu;
     private String image;
-    private String auteur;
+    private int auteur;      // FK vers utilisateur.id
     private String region;
     private String categorie;
 
     public Blog() {}
 
     public Blog(String titre, String contenu, String image,
-                String auteur, String region, String categorie) {
+                int auteur, String region, String categorie) {
         this.titre = titre;
         this.contenu = contenu;
         this.image = image;
@@ -22,8 +23,8 @@ public class Blog {
     }
 
     // GETTERS & SETTERS
-    public int getId_article() { return id_article; }
-    public void setId_article(int id_article) { this.id_article = id_article; }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getTitre() { return titre; }
     public void setTitre(String titre) { this.titre = titre; }
@@ -34,8 +35,8 @@ public class Blog {
     public String getImage() { return image; }
     public void setImage(String image) { this.image = image; }
 
-    public String getAuteur() { return auteur; }
-    public void setAuteur(String auteur) { this.auteur = auteur; }
+    public int getAuteur() { return auteur; }
+    public void setAuteur(int auteur) { this.auteur = auteur; }
 
     public String getRegion() { return region; }
     public void setRegion(String region) { this.region = region; }
