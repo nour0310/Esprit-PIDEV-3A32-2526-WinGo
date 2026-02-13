7d332b06599d1b904d2b733f4b2bffbc1df1b9be
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-02-13 22:21:25 +0100

    BlogDetails

--- WinGo/src/main/java/GUI/MainFX.java
+++ WinGo/src/main/java/GUI/MainFX.java
@@ -17,10 +17,10 @@ public class MainFX extends Application {
 
         stage.setTitle("WinGo by HexaVibe - Plateforme Touristique Intelligente");
         stage.setScene(scene);
-        stage.setMinWidth(1100);
-        stage.setMinHeight(700);
-        stage.setWidth(1100);
-        stage.setHeight(700);
+        stage.setMinWidth(1400);
+        stage.setMinHeight(500);
+        stage.setWidth(1400);
+        stage.setHeight(500);
         stage.show();
     }
 
