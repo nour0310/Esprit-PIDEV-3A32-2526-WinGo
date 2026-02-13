db787fc20e5a3b9077b4dd34b8674680df0d7380
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-02-13 14:20:15 +0100

    modification de entite

--- WinGo/src/main/java/Entites/Commentaire.java
+++ WinGo/src/main/java/Entites/Commentaire.java
@@ -3,29 +3,34 @@ package Entites;
 import java.util.Date;
 
 public class Commentaire {
-    private int id;
+    private int id_commentaire;   // PK
     private String contenu;
     private Date dateCommentaire;
-    private int blogId;
-    private String utilisateur;
+    private int id_article;       // FK vers article
+    private int utilisateur;     // FK vers utilisateur.id
 
     public Commentaire() {}
 
-    public Commentaire(String contenu, Date dateCommentaire, int blogId, String utilisateur) {
+    public Commentaire(String contenu, Date dateCommentaire, int id_article, int utilisateur) {
         this.contenu = contenu;
         this.dateCommentaire = dateCommentaire;
-        this.blogId = blogId;
+        this.id_article = id_article;
         this.utilisateur = utilisateur;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    // GETTERS & SETTERS
+    public int getId_commentaire() { return id_commentaire; }
+    public void setId_commentaire(int id_commentaire) { this.id_commentaire = id_commentaire; }
+
     public String getContenu() { return contenu; }
     public void setContenu(String contenu) { this.contenu = contenu; }
+
     public Date getDateCommentaire() { return dateCommentaire; }
     public void setDateCommentaire(Date dateCommentaire) { this.dateCommentaire = dateCommentaire; }
-    public int getBlogId() { return blogId; }
-    public void setBlogId(int blogId) { this.blogId = blogId; }
-    public String getUtilisateur() { return utilisateur; }
-    public void setUtilisateur(String utilisateur) { this.utilisateur = utilisateur; }
+
+    public int getId_article() { return id_article; }
+    public void setId_article(int id_article) { this.id_article = id_article; }
+
+    public int getUtilisateur() { return utilisateur; }
+    public void setUtilisateur(int utilisateur) { this.utilisateur = utilisateur; }
 }
