267413fca207df4304c20d33dc5d3591dc64e735
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-02-13 15:35:34 +0100

    Controllers

--- WinGo/src/main/java/Tests/MainConnection.java
+++ WinGo/src/main/java/Tests/MainConnection.java
@@ -1,11 +1,10 @@
 package Tests;
 
 import Entites.Blog;
-import Entites.Commentaire;  // ← important !
+import Entites.Commentaire;
 import Utils.MyBD;
 
 import java.sql.SQLException;
-import java.util.Date;
 
 public class MainConnection {
 
@@ -13,16 +12,16 @@ public class MainConnection {
 
         MyBD myBD = MyBD.getInstance();
 
-        // Création des objets Blog
-        Blog b1 = new Blog("Sahara Adventure", "Voyage inoubliable à Douz", "douz.jpg", "Admin", "Sud", "Voyage");
-        Blog b2 = new Blog("Carthage Story", "Découverte historique de Carthage", "carthage.jpg", "Admin", "Nord", "Histoire");
+        // Création des objets Blog (auteur = ID utilisateur existant, par exemple 1)
+        Blog b1 = new Blog("Sahara Adventure", "Voyage inoubliable à Douz", "douz.jpg", "Sud", "Voyage", 1);
+        Blog b2 = new Blog("Carthage Story", "Découverte historique de Carthage", "carthage.jpg", "Nord", "Histoire", 1);
 
-        // Création des objets Commentaire
-        Commentaire c1 = new Commentaire("Super article !", new Date(), b1.getId_article(), 1); // 1 = id utilisateur fictif
-        Commentaire c2 = new Commentaire("J'adore cet endroit !", new Date(), b2.getId_article(), 2); // 2 = id utilisateur fictif
+        // Création des objets Commentaire (sans date, elle sera générée automatiquement par la base)
+        Commentaire c1 = new Commentaire("Super article !", 1, b1.getId());
+        Commentaire c2 = new Commentaire("J'adore cet endroit !", 2, b2.getId());
 
-        // Exemple d'affichage
-        System.out.println(b1.getTitre());
-        System.out.println(c1.getContenu());
+        // Affichage
+        System.out.println("Titre du blog : " + b1.getTitre());
+        System.out.println("Commentaire : " + c1.getContenu());
     }
 }
