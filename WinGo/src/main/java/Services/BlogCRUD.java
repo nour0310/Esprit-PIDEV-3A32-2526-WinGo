4efb22871f4c50ac1d3667ff4e869a6a6c0d4edc
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-02-13 15:28:31 +0100

    modification BlogCRUD

--- WinGo/src/main/java/Services/BlogCRUD.java
+++ WinGo/src/main/java/Services/BlogCRUD.java
@@ -4,45 +4,52 @@ import Entites.Blog;
 import Utils.MyBD;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BlogCRUD implements IntrefaceCRUD<Blog> {
 
-    Connection conn;
+    private Connection conn;
 
     public BlogCRUD() {
         conn = MyBD.getInstance().getConn();
     }
 
     @Override
-    public void ajouter(Blog b) throws SQLException {
-        String req = "INSERT INTO article (titre, contenu, image, auteur, region, categorie) " +
-                "VALUES ('" + b.getTitre() + "', '" + b.getContenu() + "', '" + b.getImage() + "', '" +
-                b.getAuteur() + "', '" + b.getRegion() + "', '" + b.getCategorie() + "')";
-        Statement st = conn.createStatement();
-        st.executeUpdate(req);
+    public void ajouter(Blog blog) throws SQLException {
+        String req = "INSERT INTO article (titre, contenu, image, region, categorie, auteur, date_publication) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement pst = conn.prepareStatement(req);
+        pst.setString(1, blog.getTitre());
+        pst.setString(2, blog.getContenu());
+        pst.setString(3, blog.getImage());
+        pst.setString(4, blog.getRegion());
+        pst.setString(5, blog.getCategorie());
+        pst.setInt(6, blog.getAuteur());
+        pst.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
+        pst.executeUpdate();
         System.out.println("Blog ajouté !");
     }
 
     @Override
-    public void modifier(Blog b) throws SQLException {
-        String req = "UPDATE article SET titre=?, contenu=?, image=?, auteur=?, region=?, categorie=? WHERE id_article=?";
+    public void modifier(Blog blog) throws SQLException {
+        String req = "UPDATE article SET titre=?, contenu=?, image=?, region=?, categorie=?, auteur=? WHERE id=?";
         PreparedStatement pst = conn.prepareStatement(req);
-        pst.setString(1, b.getTitre());
-        pst.setString(2, b.getContenu());
-        pst.setString(3, b.getImage());
-        pst.setString(4, b.getAuteur());
-        pst.setString(5, b.getRegion());
-        pst.setString(6, b.getCategorie());
-        pst.setInt(7, b.getId_article());
+        pst.setString(1, blog.getTitre());
+        pst.setString(2, blog.getContenu());
+        pst.setString(3, blog.getImage());
+        pst.setString(4, blog.getRegion());
+        pst.setString(5, blog.getCategorie());
+        pst.setInt(6, blog.getAuteur());
+        pst.setInt(7, blog.getId());
         pst.executeUpdate();
         System.out.println("Blog modifié !");
     }
 
     @Override
     public void supprimer(int id) throws SQLException {
-        String req = "DELETE FROM article WHERE id_article=?";
+        String req = "DELETE FROM article WHERE id=?";
         PreparedStatement pst = conn.prepareStatement(req);
         pst.setInt(1, id);
         pst.executeUpdate();
@@ -51,24 +58,50 @@ public class BlogCRUD implements IntrefaceCRUD<Blog> {
 
     @Override
     public List<Blog> afficher() throws SQLException {
-        String req = "SELECT * FROM article ORDER BY id_article DESC";
+        String req = "SELECT a.*, u.nom, u.prenom FROM article a " +
+                "LEFT JOIN utilisateur u ON a.auteur = u.id ORDER BY a.date_publication DESC";
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery(req);
-        List<Blog> listeBlogs = new ArrayList<>();
+        List<Blog> liste = new ArrayList<>();
 
         while (rs.next()) {
             Blog b = new Blog();
-            b.setId_article(rs.getInt("id_article"));
+            b.setId(rs.getInt("id"));
             b.setTitre(rs.getString("titre"));
             b.setContenu(rs.getString("contenu"));
+            b.setDatePublication(rs.getTimestamp("date_publication").toLocalDateTime());
             b.setImage(rs.getString("image"));
-            b.setAuteur(rs.getString("auteur"));
             b.setRegion(rs.getString("region"));
             b.setCategorie(rs.getString("categorie"));
-
-            listeBlogs.add(b);
+            b.setAuteur(rs.getInt("auteur"));
+            // nom complet de l'auteur
+            String auteurNom = rs.getString("nom") + " " + rs.getString("prenom");
+            b.setAuteurNom(auteurNom);
+            liste.add(b);
         }
+        return liste;
+    }
 
-        return listeBlogs;
+    // Méthode pour récupérer un blog par son ID (optionnelle)
+    public Blog getById(int id) throws SQLException {
+        String req = "SELECT a.*, u.nom, u.prenom FROM article a " +
+                "LEFT JOIN utilisateur u ON a.auteur = u.id WHERE a.id=?";
+        PreparedStatement pst = conn.prepareStatement(req);
+        pst.setInt(1, id);
+        ResultSet rs = pst.executeQuery();
+        if (rs.next()) {
+            Blog b = new Blog();
+            b.setId(rs.getInt("id"));
+            b.setTitre(rs.getString("titre"));
+            b.setContenu(rs.getString("contenu"));
+            b.setDatePublication(rs.getTimestamp("date_publication").toLocalDateTime());
+            b.setImage(rs.getString("image"));
+            b.setRegion(rs.getString("region"));
+            b.setCategorie(rs.getString("categorie"));
+            b.setAuteur(rs.getInt("auteur"));
+            b.setAuteurNom(rs.getString("nom") + " " + rs.getString("prenom"));
+            return b;
+        }
+        return null;
     }
 }
