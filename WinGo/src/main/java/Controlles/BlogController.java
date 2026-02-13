c343a4d87b7eca0c8ee95324c6208c19a850f5af
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-02-13 13:13:44 +0100

    controller

--- WinGo/src/main/java/Controlles/BlogController.java
+++ WinGo/src/main/java/Controlles/BlogController.java
@@ -475,3 +475,11 @@ public class BlogController {
         }
     }
 
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
