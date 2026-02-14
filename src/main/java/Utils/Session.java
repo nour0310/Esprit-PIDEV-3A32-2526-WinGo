fdb5684ca391294fd27f61e0e44dd65ba26cd5a1
Yomna Helali <yomna.helali@esprit.tn>
2026-02-14 16:45:51 +0100

    integrer avec user + produit

--- src/main/java/Utils/Session.java
+++ src/main/java/Utils/Session.java
@@ -1,4 +1,34 @@
 package Utils;
 
-public class Session {
+public final class Session {
+    private static int currentUserId = -1;
+    private static String currentUserType = null; // CLIENT / COMMERCANT / ADMIN
+
+    private Session() {}
+
+    public static void setUser(int userId, String type) {
+        currentUserId = userId;
+        currentUserType = type;
+    }
+
+    public static int getUserId() {
+        return currentUserId;
+    }
+
+    public static String getUserType() {
+        return currentUserType;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUserId > 0;
+    }
+
+    public static boolean isCommercant() {
+        return "COMMERCANT".equalsIgnoreCase(currentUserType);
+    }
+
+    public static void clear() {
+        currentUserId = -1;
+        currentUserType = null;
+    }
 }
