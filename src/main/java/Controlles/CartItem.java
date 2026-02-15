d5fc45ca77682a4ffb3909ec10311fc033655b7d
Yomna Helali <yomna.helali@esprit.tn>
2026-02-15 20:37:00 +0100

    reglage design panier

--- src/main/java/Controlles/CartItem.java
+++ src/main/java/Controlles/CartItem.java
@@ -3,18 +3,21 @@ package Controlles;
 public class CartItem {
     private final int idProduit;
     private final String nom;
+    private final String image; // ✅ new
     private final double prix;
     private int qty;
 
-    public CartItem(int idProduit, String nom, double prix, int qty) {
+    public CartItem(int idProduit, String nom, String image, double prix, int qty) {
         this.idProduit = idProduit;
         this.nom = nom;
+        this.image = image;
         this.prix = prix;
         this.qty = qty;
     }
 
     public int getIdProduit() { return idProduit; }
     public String getNom() { return nom; }
+    public String getImage() { return image; }   // ✅ new
     public double getPrix() { return prix; }
     public int getQty() { return qty; }
 
