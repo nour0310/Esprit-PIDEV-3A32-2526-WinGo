7d0ef75dc6d9e62fa4d311fc15483c89eca264c8
logddd <134465158+logddd@users.noreply.github.com>
2026-02-16 08:00:06 +0100

    commit

--- workshop3A9/sql_add_admin.sql
+++
@@ -1,7 +0,0 @@
--- Add admin account to log in
--- Run this in MySQL
-
-USE wingo;
-
-INSERT INTO utilisateur (nom, prenom, email, mot_de_passe, type, telephone, age)
-VALUES ('Admin', 'User', 'admin@wingo.com', 'admin123', 'admin', '12345678', 30);
--- workshop3A9/sql_fix_type_column.sql
+++
@@ -1,7 +0,0 @@
--- Fix "Data truncated for column 'type'" error
--- Run this in MySQL: USE wingo; then run the ALTER below
-
-USE wingo;
-
--- Make type column accept 'user', 'admin', etc. (VARCHAR 50)
-ALTER TABLE utilisateur MODIFY COLUMN type VARCHAR(50);
