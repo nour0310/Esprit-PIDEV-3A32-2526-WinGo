9fc6b5c39a7e746da751372c005fa9d5b33cc28c
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-02-21 23:59:07 +0100

    ajout des metiers avancees

--- WinGo/src/main/java/Services/External/LibreTranslateService.java
+++ WinGo/src/main/java/Services/External/LibreTranslateService.java
@@ -2,6 +2,7 @@ package Services.External;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -13,7 +14,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class LibreTranslateService {
 
-    private static final String API_URL = "https://libretranslate.de/translate";
+    // Utiliser une instance alternative si celle-ci est instable
+    private static final String API_URL = "https://translate.argosopentech.com/translate";
     private static final Gson gson = new Gson();
 
     public static CompletableFuture<String> translateAsync(String text, String sourceLang, String targetLang) {
@@ -30,9 +32,19 @@ public class LibreTranslateService {
 
                 post.setEntity(new StringEntity(gson.toJson(json), "UTF-8"));
 
+                System.out.println("Envoi requête à " + API_URL + " avec texte: " + text.substring(0, Math.min(20, text.length())) + "...");
+
                 try (CloseableHttpResponse response = client.execute(post)) {
+                    int statusCode = response.getStatusLine().getStatusCode();
                     String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
-                    JsonObject responseJson = gson.fromJson(responseBody, JsonObject.class);
+                    System.out.println("Statut: " + statusCode + ", Réponse: " + responseBody);
+
+                    if (statusCode != 200) {
+                        System.err.println("Erreur API: " + statusCode + " - " + responseBody);
+                        return text;
+                    }
+
+                    JsonObject responseJson = JsonParser.parseString(responseBody).getAsJsonObject();
                     return responseJson.get("translatedText").getAsString();
                 }
             } catch (Exception e) {
