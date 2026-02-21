6281a0462cddc739f2f72fd0ea5e006836dbd4d6
logddd <134465158+logddd@users.noreply.github.com>
2026-02-21 13:49:01 +0100

    commit

--- src/main/java/GUI/myconnection.java
+++ src/main/java/GUI/myconnection.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class myconnection extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Reservation.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/accueil.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setTitle("WinGo Travel");
         stage.setScene(scene);
