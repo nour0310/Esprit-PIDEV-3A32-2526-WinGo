e2a38a213aae1c9c8be479c1a0e9e5083be97b1c
yomna helali <yomna.helali@esprit.tn>
2026-04-01 23:38:26 +0100

    creationn

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +17,28 @@ class ProduitType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('idUser')
-            ->add('nom')
-            ->add('description')
-            ->add('prix')
-            ->add('region')
-            ->add('categorie')
-            ->add('stock')
-            ->add('image')
-            ->add('dateAjout')
-        ;
+            ->add('idUser', IntegerType::class)
+            ->add('nom', TextType::class)
+            ->add('description', TextareaType::class, [
+                'required' => false
+            ])
+            ->add('prix', MoneyType::class, [
+                'currency' => false
+            ])
+            ->add('region', TextType::class, [
+                'required' => false
+            ])
+            ->add('categorie', TextType::class, [
+                'required' => false
+            ])
+            ->add('stock', IntegerType::class)
+            ->add('image', TextType::class, [
+                'required' => false
+            ])
+            ->add('dateAjout', DateTimeType::class, [
+                'required' => false,
+                'widget' => 'single_text'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
