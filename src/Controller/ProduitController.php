043306901c66c55cce811858e9357c51f31e2c3a
yomna helali <yomna.helali@esprit.tn>
2026-04-02 00:58:37 +0100

    crud

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -7,20 +7,22 @@ use App\Form\ProduitType;
 use App\Repository\ProduitRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/produit')]
 final class ProduitController extends AbstractController
 {
     #[Route('/list', name: 'produit_list')]
-public function list(ProduitRepository $repo): Response
-{
-    return $this->render('produit/list.html.twig', [
-        'list' => $repo->findAll()
-    ]);
-}
+    public function list(ProduitRepository $repo): Response
+    {
+        return $this->render('produit/list.html.twig', [
+            'list' => $repo->findAll()
+        ]);
+    }
 
     #[Route('/details/{id}', name: 'produit_details')]
     public function details($id, ProduitRepository $repo): Response
@@ -31,8 +33,11 @@ public function list(ProduitRepository $repo): Response
     }
 
     #[Route('/add', name: 'produit_add')]
-    public function add(ManagerRegistry $manager, Request $request): Response
-    {
+    public function add(
+        ManagerRegistry $manager,
+        Request $request,
+        SluggerInterface $slugger
+    ): Response {
         $em = $manager->getManager();
         $produit = new Produit();
 
@@ -40,6 +45,26 @@ public function list(ProduitRepository $repo): Response
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $imageFile = $form->get('imageFile')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    throw new \Exception('Erreur lors de l upload de l image');
+                }
+
+                $produit->setImage($newFilename);
+            }
+
             if ($produit->getDateAjout() === null) {
                 $produit->setDateAjout(new \DateTime());
             }
@@ -56,8 +81,13 @@ public function list(ProduitRepository $repo): Response
     }
 
     #[Route('/update/{id}', name: 'produit_update')]
-    public function update($id, ProduitRepository $repo, ManagerRegistry $manager, Request $request): Response
-    {
+    public function update(
+        $id,
+        ProduitRepository $repo,
+        ManagerRegistry $manager,
+        Request $request,
+        SluggerInterface $slugger
+    ): Response {
         $em = $manager->getManager();
         $produit = $repo->find($id);
 
@@ -69,7 +99,28 @@ public function list(ProduitRepository $repo): Response
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $imageFile = $form->get('imageFile')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    throw new \Exception('Erreur lors de l upload de l image');
+                }
+
+                $produit->setImage($newFilename);
+            }
+
             $em->flush();
+
             return $this->redirectToRoute('produit_list');
         }
 
