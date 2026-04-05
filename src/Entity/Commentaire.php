e768c3d5fe44e6a1512a8dd41478be8119ed6a6b
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-04-05 19:32:09 +0100

    Integration user+produit+blogs

--- src/Entity/Commentaire.php
+++ src/Entity/Commentaire.php
@@ -2,8 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\CommentaireRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use App\Repository\CommentaireRepository;
 
 #[ORM\Entity(repositoryClass: CommentaireRepository::class)]
 #[ORM\Table(name: 'commentaire')]
@@ -14,34 +17,69 @@ class Commentaire
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'text')]
-    private string $contenu = '';
+    #[ORM\Column(type: 'text', nullable: false)]
+    #[Assert\NotBlank(message: "Le commentaire ne peut pas être vide")]
+    private ?string $contenu = null;
 
-    #[ORM\Column(name: 'date_commentaire', type: 'datetime', nullable: true)]
+    #[ORM\Column(type: 'datetime', nullable: true)]
     private ?\DateTimeInterface $dateCommentaire = null;
 
-    #[ORM\ManyToOne(targetEntity: Utilisateur::class)]
-    #[ORM\JoinColumn(name: 'utilisateur', referencedColumnName: 'id', nullable: true)]
+    #[ORM\ManyToOne(targetEntity: Utilisateur::class, inversedBy: 'commentaires')]
+    #[ORM\JoinColumn(name: 'utilisateur', referencedColumnName: 'id')]
     private ?Utilisateur $utilisateur = null;
 
     #[ORM\ManyToOne(targetEntity: Article::class, inversedBy: 'commentaires')]
-    #[ORM\JoinColumn(name: 'article_id', referencedColumnName: 'id', nullable: true)]
+    #[ORM\JoinColumn(name: 'article_id', referencedColumnName: 'id')]
     private ?Article $article = null;
 
-    #[ORM\Column(name: 'parent_id', type: 'integer', nullable: true)]
-    private ?int $parentId = null;
+    #[ORM\ManyToOne(targetEntity: Commentaire::class, inversedBy: 'reponses')]
+    #[ORM\JoinColumn(name: 'parent_id', referencedColumnName: 'id')]
+    private ?Commentaire $parent = null;
 
-    public function __construct() { $this->dateCommentaire = new \DateTime(); }
+    #[ORM\OneToMany(targetEntity: Commentaire::class, mappedBy: 'parent')]
+    private Collection $reponses;
+
+    public function __construct()
+    {
+        $this->reponses = new ArrayCollection();
+        $this->dateCommentaire = new \DateTime();
+    }
 
     public function getId(): ?int { return $this->id; }
-    public function getContenu(): string { return $this->contenu; }
-    public function setContenu(string $contenu): static { $this->contenu = $contenu; return $this; }
+    public function setId(int $id): self { $this->id = $id; return $this; }
+
+    public function getContenu(): ?string { return $this->contenu; }
+    public function setContenu(string $contenu): self { $this->contenu = $contenu; return $this; }
+
     public function getDateCommentaire(): ?\DateTimeInterface { return $this->dateCommentaire; }
-    public function setDateCommentaire(?\DateTimeInterface $dateCommentaire): static { $this->dateCommentaire = $dateCommentaire; return $this; }
+    public function setDateCommentaire(?\DateTimeInterface $dateCommentaire): self { $this->dateCommentaire = $dateCommentaire; return $this; }
+
     public function getUtilisateur(): ?Utilisateur { return $this->utilisateur; }
-    public function setUtilisateur(?Utilisateur $utilisateur): static { $this->utilisateur = $utilisateur; return $this; }
+    public function setUtilisateur(?Utilisateur $utilisateur): self { $this->utilisateur = $utilisateur; return $this; }
+
     public function getArticle(): ?Article { return $this->article; }
-    public function setArticle(?Article $article): static { $this->article = $article; return $this; }
-    public function getParentId(): ?int { return $this->parentId; }
-    public function setParentId(?int $parentId): static { $this->parentId = $parentId; return $this; }
+    public function setArticle(?Article $article): self { $this->article = $article; return $this; }
+
+    public function getParent(): ?Commentaire { return $this->parent; }
+    public function setParent(?Commentaire $parent): self { $this->parent = $parent; return $this; }
+
+    /**
+     * @return Collection<int, Commentaire>
+     */
+    public function getReponses(): Collection { return $this->reponses; }
+    public function addReponse(Commentaire $reponse): self {
+        if (!$this->reponses->contains($reponse)) {
+            $this->reponses->add($reponse);
+            $reponse->setParent($this);
+        }
+        return $this;
+    }
+    public function removeReponse(Commentaire $reponse): self {
+        if ($this->reponses->removeElement($reponse)) {
+            if ($reponse->getParent() === $this) {
+                $reponse->setParent(null);
+            }
+        }
+        return $this;
+    }
 }
