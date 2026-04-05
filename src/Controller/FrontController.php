945dd5647a4b14a8fdc10ba02c68e0b0e69c7ee2
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-04-05 18:42:27 +0100

    Sauvegarde locale avant merge de produitSymfony

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FrontController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    #[Route('/index', name: 'index_page')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+
+    #[Route('/offers', name: 'offers')]
+    public function offers(): Response
+    {
+        return $this->render('offers.html.twig');
+    }
+
+    #[Route('/single-listing', name: 'single_listing')]
+    public function singleListing(): Response
+    {
+        return $this->render('single_listing.html.twig');
+    }
+}
