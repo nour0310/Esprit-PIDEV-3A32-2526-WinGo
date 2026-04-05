b3f6479a5be13cd42df6e216ec5d1d3473bb1ee9
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-04-05 11:18:33 +0100

    modification de commentaire controller

--- Desktop/web/web/src/Controller/CommentaireController.php
+++ Desktop/web/web/src/Controller/CommentaireController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Commentaire;
-use App\Form\CommentaireType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,23 +14,25 @@ class CommentaireController extends AbstractController
 {
     // Récupérer le contenu d'un commentaire (GET)
     #[Route('/commentaire/{id}/edit', name: 'app_commentaire_edit', methods: ['GET'])]
-    public function getCommentaire(Commentaire $commentaire): Response
+    public function getCommentaire(Commentaire $commentaire): JsonResponse
     {
         return $this->json(['contenu' => $commentaire->getContenu()]);
     }
 
-    // Mettre à jour un commentaire (POST)
+    // Mettre à jour un commentaire (AJAX POST)
     #[Route('/commentaire/{id}/update', name: 'app_commentaire_update', methods: ['POST'])]
-    public function update(Request $request, Commentaire $commentaire, EntityManagerInterface $em): Response
+    public function update(Request $request, Commentaire $commentaire, EntityManagerInterface $em): JsonResponse
     {
-        $form = $this->createForm(CommentaireType::class, $commentaire);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        $data = json_decode($request->getContent(), true);
+        if (isset($data['contenu'])) {
+            $commentaire->setContenu($data['contenu']);
             $em->flush();
+            return $this->json(['success' => true, 'contenu' => $commentaire->getContenu()]);
         }
-        return $this->redirectToRoute('app_article_show', ['id' => $commentaire->getArticle()->getId()]);
+        return $this->json(['success' => false], 400);
     }
 
+    // Supprimer un commentaire (POST avec redirection)
     #[Route('/commentaire/{id}/delete', name: 'app_commentaire_delete', methods: ['POST'])]
     public function delete(Request $request, Commentaire $commentaire, EntityManagerInterface $em): Response
     {
