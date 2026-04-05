31c655758f64bdf3178a84ed8edc95bcddb5cedf
yomna helali <yomna.helali@esprit.tn>
2026-04-05 13:44:24 +0100

    commercant

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -14,6 +14,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Entity\Commande;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[IsGranted('ROLE_ADMIN')]
 #[Route('/admin')]
