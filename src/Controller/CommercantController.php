9dfdb5f1e0aa02fc3c5886f6965901990a8f647c
yomna helali <yomna.helali@esprit.tn>
2026-04-10 14:34:35 +0100

    notification commerçant

--- src/Controller/CommercantController.php
+++ src/Controller/CommercantController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Service\NotificationCommerceService;
 
 final class CommercantController extends AbstractController
 {
