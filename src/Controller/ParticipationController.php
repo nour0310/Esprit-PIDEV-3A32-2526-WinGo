5b62d17e2ba28c504187e5dda9b5340a473367bf
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-04-11 12:54:32 +0100

    ajouter les sentiments des clients

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
