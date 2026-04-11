8c328d322f1da6e3e47f58a43e05c0826d90e86c
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-04-11 11:01:57 +0100

    ajouter la resume d'une article

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Ehyiah\QuillJsBundle\QuillJsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\AI\AiBundle\AiBundle::class => ['all' => true],
 ];
