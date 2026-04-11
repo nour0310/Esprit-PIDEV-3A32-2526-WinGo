c10b201c823cdaed642649bf8fe9ccc88891453e
yomna helali <yomna.helali@esprit.tn>
2026-04-11 13:51:47 +0100

    mailing

---
+++ src/Service/CommandeMailerService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Commande;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+
+class CommandeMailerService
+{
+    public function __construct(
+        private MailerInterface $mailer
+    ) {}
+
+    public function sendCommandeLivreeEmail(Utilisateur $client, Commande $commande): void
+    {
+        $email = (new TemplatedEmail())
+            ->from(new Address('noreply@wingo.tn', 'WinGo'))
+            ->to(new Address($client->getEmail(), $client->getFullName()))
+            ->subject('Votre commande a été livrée')
+            ->htmlTemplate('emails/commande_livree.html.twig')
+            ->context([
+                'client' => $client,
+                'commande' => $commande,
+            ]);
+
+        $this->mailer->send($email);
+    }
+
+    public function sendCommandeAnnuleeEmail(Utilisateur $client, Commande $commande): void
+    {
+        $email = (new TemplatedEmail())
+            ->from(new Address('noreply@wingo.tn', 'WinGo'))
+            ->to(new Address($client->getEmail(), $client->getFullName()))
+            ->subject('Votre commande a été annulée')
+            ->htmlTemplate('emails/commande_annulee.html.twig')
+            ->context([
+                'client' => $client,
+                'commande' => $commande,
+                'cause' => $commande->getCauseAnnulation(),
+            ]);
+
+        $this->mailer->send($email);
+    }
+}
