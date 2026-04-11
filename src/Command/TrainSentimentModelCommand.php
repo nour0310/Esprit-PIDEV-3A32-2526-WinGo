06c5e8f7ef82da299b3e551038035ee5fe12c4ce
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-04-11 12:22:00 +0100

    ajouter les sentiments des clients

--- src/Command/TrainSentimentModelCommand.php
+++ src/Command/TrainSentimentModelCommand.php
@@ -2,13 +2,14 @@
 
 namespace App\Command;
 
-use Rubix\ML\Classifiers\NaiveBayes;
+use Rubix\ML\Classifiers\GaussianNB;
 use Rubix\ML\Datasets\Labeled;
 use Rubix\ML\PersistentModel;
 use Rubix\ML\Persisters\Filesystem;
 use Rubix\ML\Pipeline;
 use Rubix\ML\Transformers\WordCountVectorizer;
 use Rubix\ML\Transformers\TextNormalizer;
+use Rubix\ML\Transformers\TfIdfTransformer;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -66,8 +67,9 @@ class TrainSentimentModelCommand extends Command
         $estimator = new PersistentModel(
             new Pipeline([
                 new TextNormalizer(),
-                new WordCountVectorizer(),
-            ], new NaiveBayes()),
+                new WordCountVectorizer(10000),
+                new TfIdfTransformer(),
+            ], new GaussianNB()),
             new Filesystem($this->projectDir . '/var/ml/sentiment.model')
         );
 
