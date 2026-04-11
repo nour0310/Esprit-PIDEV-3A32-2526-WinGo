8bb265eb41c93652907d5a04adcbb52cd74d1c27
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-04-11 13:49:36 +0100

    ajouter les sentiments des clients

--- src/Command/TrainSentimentModelCommand.php
+++ src/Command/TrainSentimentModelCommand.php
@@ -2,7 +2,7 @@
 
 namespace App\Command;
 
-use Rubix\ML\Classifiers\ComplementNB;
+use Rubix\ML\Classifiers\NaiveBayes;
 use Rubix\ML\Datasets\Labeled;
 use Rubix\ML\PersistentModel;
 use Rubix\ML\Persisters\Filesystem;
