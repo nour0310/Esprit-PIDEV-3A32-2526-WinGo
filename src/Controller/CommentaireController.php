0344db59a4e301de35b2fac7cb25fa1fbcd6c689
Balkis-zgh <balkis.zaghdoud@esprit.tn>
2026-04-17 12:18:38 +0100

    slug

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class CommentaireController extends AbstractController
 {
@@ -26,17 +27,20 @@ class CommentaireController extends AbstractController
     #[Route('/commentaire/{id}/delete', name: 'app_commentaire_delete', methods: ['POST'])]
     public function delete(Request $request, Commentaire $commentaire, EntityManagerInterface $em): Response
     {
-        $articleId = $commentaire->getArticle()->getId();
+        $article = $commentaire->getArticle();
+        $articleId = $article->getId();
+        $slugger = new AsciiSlugger();
+        $slug = $slugger->slug($article->getTitre())->lower()->toString();
 
         if ($this->getUser() !== $commentaire->getUtilisateur() && !$this->isGranted('ROLE_ADMIN')) {
-             return $this->redirectToRoute('app_article_show', ['id' => $articleId]);
+             return $this->redirectToRoute('app_article_show', ['id' => $articleId, 'slug' => $slug]);
         }
 
         if ($this->isCsrfTokenValid('delete' . $commentaire->getId(), $request->request->get('_token'))) {
             $em->remove($commentaire);
             $em->flush();
         }
-        return $this->redirectToRoute('app_article_show', ['id' => $articleId]);
+        return $this->redirectToRoute('app_article_show', ['id' => $articleId, 'slug' => $slug]);
     }
 
     #[Route('/commentaire/{id}/sentiment', name: 'app_commentaire_sentiment', methods: ['GET'])]
